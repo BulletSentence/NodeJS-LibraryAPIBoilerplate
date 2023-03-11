76a4eef973f9505b7f8824272e7cc028e95894f2
Leonardo <leonardo.a@acad.ifma.edu.br>
2023-03-11 10:00:40 -0300

    Files updated

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const http = require('http');
 const port = 3000;
 
+const rotas = {
+  '/' : 'Home',
+  '/books': 'Livros',
+  '/authors': 'Autores',
+  '/publishers' : 'Editora',
+  '/about': 'Sobre'
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200,  {'Content-Type': 'text/plain'})
-  res.end('Curso Test')
+  res.end(rotas[req.url])
 });
 
 server.listen(port, () => {
     console.log("Servidor em: http://localhost:" + port)
-})
+});
+
